c2d3e14ef88d915c1fcf370f07f03695970f2455
MliZZ1 <152088336+MliZZ1@users.noreply.github.com>
2024-02-22 21:54:16 +0100

    Create installonarch.sh

---
+++ installonarch.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/MliZZ1/Minimal-Dotfiles
+cd Minimal-Dotfiles
+cp alacritty/alacritty.yml .config/alacritty/alacritty.yml
+cp rofi/config.rasi .config/rofi/config.rasi
+cp rofi/launcher.rasi .config/rofi/launcher.rasi
+cp bspwm/bspwmrc .config/bspwm/bspwmrc
