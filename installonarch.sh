7bce85f91c56233c66c7445cdfa04458d8497e7e
MliZZ1 <152088336+MliZZ1@users.noreply.github.com>
2024-08-08 09:55:48 +0000

    Update installonarch.sh

--- installonarch.sh
+++ installonarch.sh
@@ -1,5 +1,3 @@
-git clone https://github.com/MliZZ1/Minimal-Dotfiles
-cd Minimal-Dotfiles
 cp alacritty/alacritty.yml .config/alacritty/alacritty.yml
 cp rofi/config.rasi .config/rofi/config.rasi
 cp rofi/launcher.rasi .config/rofi/launcher.rasi
